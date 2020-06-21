37c30f429381203c8492e424eab01073d6a7c809
dakota <dakota.f.peck@protonmail.com>
2020-06-20 18:12:10 -0600

    Improved error checking

--- src/main/java/database/DbUtils.java
+++ src/main/java/database/DbUtils.java
@@ -3,6 +3,7 @@ package ivory.database;
 import org.h2.tools.RunScript;
 
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,13 +13,10 @@ public class DbUtils
   public static ResultSet runScriptFromResource(Class<?> cls, Connection conn, String resourcePath)
     throws SQLException
   {
-    try
-    {
-      return RunScript.execute(conn, new InputStreamReader(cls.getResourceAsStream(resourcePath)));
-    }
-    catch (NullPointerException e)
-    {
+    InputStream resourceStream = cls.getResourceAsStream(resourcePath);
+    if (resourceStream == null)
       throw new NullPointerException("No such resource '" + resourcePath + "' accessible from " + cls);
-    }
+
+    return RunScript.execute(conn, new InputStreamReader(resourceStream));
   }
 }
